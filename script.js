1bb4064b912a5307028526dd3931579c8d8ac1e2
janhav-i2006 <janhvithorve7@gmail.com>
2026-05-28 20:38:29 +0530

    First commit

--- script.js
+++ script.js
@@ -1,73 +1,43 @@
 const circles = document.querySelectorAll('.circle');
 
 circles.forEach(circle => {
+
     let percent = circle.getAttribute('data-percent');
     let color = circle.getAttribute('data-color');
     let count = 0;
 
     let interval = setInterval(() => {
-        if (count >= percent) {
+
+        if(count >= percent){
+
             clearInterval(interval);
+
         } else {
+
             count++;
 
-            // ring animation
-            circle.style.background = `conic-gradient(${color} ${count * 3.6}deg, #1e293b 0deg)`;
+            circle.style.background =
+            `conic-gradient(${color} ${count * 3.6}deg, #1e293b 0deg)`;
 
-            // moving percentage text
             circle.querySelector("span").innerText = count + "%";
         }
-    }, 20); // speed control
-});
-function toggleMenu() {
-  let menu = document.getElementById("menu");
 
-  if (menu.style.right === "0px") {
-    menu.style.right = "-250px";
-  } else {
-    menu.style.right = "0px";
-  }
+    },20);
 
-const circles = document.querySelectorAll('.circle');
-
-circles.forEach(circle => {
-    let percent = circle.getAttribute('data-percent');
-    let color = circle.getAttribute('data-color');
-    let count = 0;
-
-    let interval = setInterval(() => {
-        if (count >= percent) {
-            clearInterval(interval);
-        } else {
-            count++;
-
-            // ring animation
-            circle.style.background = `conic-gradient(${color} ${count * 3.6}deg, #1e293b 0deg)`;
-
-            // moving percentage text
-            circle.querySelector("span").innerText = count + "%";
-        }
-    }, 20); // speed control
 });
 
-function toggleMenu() {
-  let menu = document.getElementById("menu");
+/* ===== FUNNY RESUME ===== */
 
-  if (menu.style.right === "0px") {
-    menu.style.right = "-250px";
-  } else {
-    menu.style.right = "0px";
-  }
-}
-}
-function funnyResume() {
+function funnyResume(){
 
     let audio = new Audio("funny.mp4");
 
     audio.play();
 
     setTimeout(() => {
+
         alert("😂 Aabhi nahi milega!");
-    }, 500);
+
+    },500);
 
 }
